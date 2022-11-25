c0bdec4eeedca55e0989b3d56c38b38f227b1e96
HenriBDev <henribdev@gmail.com.br>
2022-11-25 02:48:43 -0300

    Adicionada tela secundária e melhorado filtros de Logradouro e Bairro

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,114 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(
-    MyApp(),
-  );
+List<Venda> baseDados = [];
+String? host_ip = '';
+
+void main() async{
+  await dotenv.load(fileName: ".env");
+  host_ip = dotenv.env['HOST_IP'];
+  baseDados = await HttpHelper().lerDados();
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.deepPurple,
+    ),
+    home: TelaPrincipal()
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class Venda {
+  String logradouro;
+  String bairro;
+  int preco;
+  int numero;
+  int area;
+  int quartos;
+  int banheiros;
+  int vagas;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      // Aqui instanciamos meu cronômetro
-      home: TelaPrincipal(), //Cronometro(),
-      initialRoute: '/',
-      // routes: {
-      //   '/cronometro': (context) => Cronometro(),
-      // },
-    );
+  Venda(this.logradouro, this.bairro, this.preco, this.numero, this.area, this.quartos, this.banheiros, this.vagas);
+  Venda.fromJson(Map<String, dynamic> json) :
+        logradouro = json["Logradouro"],
+        bairro = json["Bairro"],
+        preco = json["Preço (R\$)"],
+        numero = json["Número"],
+        area = json["Máximo Área (m²)"],
+        quartos = json["Máximo Quartos"],
+        banheiros = json["Máximo Banheiros"],
+        vagas = json["Máximo Vagas"];
+
+  String getFiltro(String filtro){
+    if(filtro == 'logradouro'){
+      return logradouro;
+    }
+    else if(filtro == 'bairro'){
+      return bairro;
+    }
+    return '';
   }
 }
 
-class DropdownFiltro extends StatefulWidget{
-  const DropdownFiltro({super.key});
+class HttpHelper{
+  final String dominio = (host_ip as String) + ':8080';
+  final String caminho = 'lerDados';
 
-  @override
-  State<DropdownFiltro> createState() => _DropdownFiltroState();
+  Future<List<Venda>> lerDados() async {
+    final List<Venda> vendas = [];
+
+    Uri url = Uri.http(dominio, caminho);
+    http.Response result = await http.post(url);
+
+    // Se o acesso ao web service for bem-sucedido.
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(utf8.decode(result.bodyBytes));
+      for (var response in jsonResponse) {
+        vendas.add(Venda.fromJson(response));
+      }
+      return vendas;
+    } else {
+      return vendas;
+    }
+  }
 }
 
-class _DropdownFiltroState extends State<DropdownFiltro> {
-  String dropdownValue = "Logradouros";
+class CheckboxFiltro extends StatefulWidget{
+  const CheckboxFiltro({super.key});
+
+  @override
+  State<CheckboxFiltro> createState() => _CheckboxFiltroState();
+}
+
+class _CheckboxFiltroState extends State<CheckboxFiltro> {
+  String checkboxValue = '';
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      onChanged: (String? value) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (bool? value) {
         setState(() {
-          dropdownValue = value!;
+          isChecked = value!;
         });
       },
-      items: <DropdownMenuItem<String>>[
-        DropdownMenuItem<String>(
-          value: "Logradouros",
-          child: Text("Logradouros"),
-        ),
-        DropdownMenuItem<String>(
-          value: "Bairros",
-          child: Text("Bairros"),
-        )],
     );
   }
 }
 
-class TelaPrincipal extends StatelessWidget {
-  List<String> bairros = <String>['Teste', 'Teste'];
+class TelaPrincipal extends StatefulWidget {
+
+  const TelaPrincipal({super.key});
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal>{
+  String dropdownValue = 'logradouro';
+  List<ListTile> listaFiltro = baseDados.map((venda) => ListTile(leading: CheckboxFiltro(), title: Text(venda.logradouro))).toList();
 
   @override
   Widget build(BuildContext context) {
@@ -70,9 +117,12 @@ class TelaPrincipal extends StatelessWidget {
         title: Text('Imóveis em São Paulo'),
       ),
       body: Center(
-
-        // colocar coisas da pagina central aqui
-
+        child: ElevatedButton(
+          child: Text('Créditos'),
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => TelaSecundaria()));
+          },
+        )
       ),
       drawer: Drawer(child: _criarMenuDrawer(context)),
     );
@@ -80,24 +130,63 @@ class TelaPrincipal extends StatelessWidget {
 
   Widget _criarMenuDrawer(BuildContext context) {
     return ListView(
-      children: [
-        ListTile(
-          title: DropdownFiltro()
-        ),
-        ListView.builder(
-          shrinkWrap: true,
-          physics: ClampingScrollPhysics(),
-          itemCount: bairros.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Icon(Icons.square),
-              title: Text(bairros[index]),
-            );
-          },
-        )
-      ],
-    );
+        children: [
+          ListTile(
+              title: DropdownButton<String>(
+                value: dropdownValue,
+                onChanged: (String? value) {
+                  setState(() {
+                    dropdownValue = value!;
+                    listaFiltro = baseDados.map((venda) => ListTile(leading: CheckboxFiltro(), title: Text(venda.getFiltro(value!)))).toList();
+                  });
+                },
+                items: <DropdownMenuItem<String>>[
+                  DropdownMenuItem<String>(
+                    value: "logradouro",
+                    child: Text("Logradouros"),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: "bairro",
+                    child: Text("Bairros"),
+                  )
+                ],
+              )
+          ),
+          ListView(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            children: listaFiltro,
+          ),
+        ]
+      );
   }
 }
 
-//onTap: ()=> widget.scaffoldKey.currentState.openDrawer(),
+class TelaSecundaria extends StatelessWidget {
+  const TelaSecundaria({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: ElevatedButton(
+          child: Icon(Icons.arrow_left),
+          onPressed: (){
+            Navigator.pop(context);
+          }
+        ),
+        title: Text('Créditos'),
+      ),
+      body: Center(
+        child: Text(
+          'Trabalho desenvolvido por:\nGilberto de Melo Junior\nHenrique Barbosa\nMarcus Gustavo Sigrist Romão',
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18
+          ),
+        )
+      )
+    );
+  }
+}
